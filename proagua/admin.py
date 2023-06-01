d9fd33c41e3b418cc13f33de694f52ed0136115b
Rabbithy <thiago.coelho158@gmail.com>
2023-05-31 22:19:00 -0300

    Create models Edificações and PontoColeta

--- proagua/admin.py
+++ proagua/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Edificacao)
+admin.site.register(models.PontoColeta)
--- proagua/models.py
+++ proagua/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Edificacao(models.Model):
+    codigo = models.CharField(
+        verbose_name='código',
+        max_length=20
+    )
+    nome = models.CharField(
+        verbose_name='nome da edificação',
+        max_length=80
+    )
+
+
+class PontoColeta(models.Model):
+    edificacao = models.ForeignKey(
+        to=Edificacao,
+        verbose_name='edificação',
+        on_delete=models.CASCADE
+    )
+    ambiente = models.CharField(
+        verbose_name='ambiente',
+        max_length=120
+    )
+    torneira = models.BooleanField(
+        verbose_name='torneira'
+    )
+
+    def __str__(self):
+        return f'Ponto de Coleta - {self.edificacao.nome} - {self.ambiente}'
