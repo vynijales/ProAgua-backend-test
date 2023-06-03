65ce03b0892e045b0acfba006c7b1c62b1573862
vynijales <vynijales@gmail.com>
2023-06-03 02:27:18 -0300

    Create "Coleta" Model

--- proagua/models.py
+++ proagua/models.py
@@ -1,34 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Edificacao(models.Model):
     codigo = models.CharField(
         verbose_name='código',
-        max_length=20
+        max_length=20,
     )
     nome = models.CharField(
         verbose_name='nome da edificação',
-        max_length=80
+        max_length=80,
     )
 
     def __str__(self):
         return f'{self.codigo} - {self.nome}'
 
-
 class PontoColeta(models.Model):
     edificacao = models.ForeignKey(
         to=Edificacao,
         verbose_name='edificação',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     ambiente = models.CharField(
         verbose_name='ambiente',
-        max_length=120
+        max_length=120,
     )
     torneira = models.BooleanField(
-        verbose_name='torneira'
+        verbose_name='torneira',
     )
 
     def __str__(self):
         return f'{self.edificacao.nome} - {self.ambiente}'
+
+class Coleta(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name="ID",
+        editable=False,
+    )
+    ponto_coleta = models.ForeignKey(
+        to=PontoColeta,
+        verbose_name='ponto de coleta',
+        on_delete=models.CASCADE,
+    )
+
+    date = models.DateTimeField(
+        verbose_name="data da coleta",
+        db_comment="Data e hora de quando foi realizada a coleta",
+    )
+
+    responsavel = models.ManyToManyField(
+        to=User,
+        verbose_name="responsáveis",
+    )
+
+    ordem = models.CharField(
+        max_length=1,
+        choices=(
+            ("C", "Coleta"),
+            ("R", "Recoleta")
+        ),
+    )
+
+    amostragem = models.PositiveIntegerField(
+        verbose_name="amostragem",
+    )
+
+    def __str__(self):
+        return f'{self.id}{self.ordem} - {self.date}'
