e0e62ced04e9470bf5fa125e6ade4491fabeb610
Rabbithy <thiago.coelho158@gmail.com>
2023-06-06 23:37:55 -0300

    Create ponto_coleta_relatorio view

--- proagua/views.py
+++ proagua/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import PontoColeta
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+
+from .models import (
+    PontoColeta
+)
 
 def home(request):
     return render(
@@ -16,5 +20,48 @@ def pontos_coletas(request):
     return render(
         request=request,
         template_name="privado/pontos_coletas.html",
+        context=context
+    )
+
+
+def ponto_coleta_relatorio(request, ponto_id: int, amostragem: int):
+    ponto = get_object_or_404(
+        PontoColeta,
+        id=ponto_id
+    )
+    
+    pontos = []
+    
+    while ponto != None:
+        coletas = ponto.coletas.filter(amostragem=amostragem)
+        
+        if coletas.count() == 0:
+            break
+
+        pontos.append({
+            "edificacao": ponto.edificacao,
+            "ambiente": ponto.ambiente,
+            "coletas": coletas
+        })
+        ponto = ponto.pai
+    
+    context = {
+        "pontos": pontos
+    }
+
+    return render(
+        request=request,
+        template_name="privado/ponto_coleta.html",
+        context=context
+    )
+
+
+def configuracoes(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(
+        request=request,
+        template_name="privado/configuracoes.html",
         context=context
     )
