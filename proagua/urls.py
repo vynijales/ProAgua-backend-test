f5ed0388055b3440effa8f455c23a0e129906bfa
Rabbithy <thiago.coelho158@gmail.com>
2023-06-06 23:36:16 -0300

    Update urls.py

--- proagua/urls.py
+++ proagua/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
-from .api import api
+
 
 urlpatterns = [
     path('', views.home),
     path('pontos_coletas/', views.pontos_coletas),
+    path('ponto_coleta/<int:ponto_id>/<int:amostragem>', views.ponto_coleta_relatorio),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
