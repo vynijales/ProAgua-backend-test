9f8e6f936655f5a80f77eac9bf45727636b08fbe
Rabbithy <thiago.coelho158@gmail.com>
2023-06-13 11:03:15 -0300

    Close dialog when click on backdrop (outside)

--- proagua/static/js/dialog.js
+++ proagua/static/js/dialog.js
@@ -1,11 +1,23 @@
-const backdrop = document.getElementById('dialog-backdrop')
+const backdrop = document.getElementById('dialog-backdrop');
+var currentDialog = '';
 
 function closeDialog(element_id) {
-    document.getElementById(element_id).hidden = true;
+    var el = document.getElementById(element_id)
+    if (el !== null) {
+        el.hidden = true;
+        currentDialog = element_id;
+    }
     backdrop.hidden = true;
 }
 
 function openDialog(element_id) {
     document.getElementById(element_id).hidden = false;
     backdrop.hidden = false;
+    currentDialog = '';
 }
+
+backdrop.addEventListener('click', (e) => {
+    if (e.target === backdrop) {
+        closeDialog(currentDialog);
+    }
+})
