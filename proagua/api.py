a936f932ec0bc1e90687a338729b78fbfc2d920d
vynijales <vynijales@gmail.com>
2023-08-16 16:34:50 -0300

    update: api edificacoes

--- proagua/api.py
+++ proagua/api.py
@@ -1,25 +1,29 @@
 from typing import List
 
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from ninja import NinjaAPI
+from ninja.security import django_auth
 
 from .schema import *
 from . import models
 
-api = NinjaAPI()
-
-@api.post("/edificacoes")
-def create_edificacao(request, payload: EdificacaoIn):
-    edificacao = models.Edificacao.objects.create(**payload.dict())
-    return {
-        'codigo': edificacao.codigo,
-        'nome': edificacao.nome,
-        'campus': edificacao.nome}
+api = NinjaAPI(auth=django_auth, csrf=True)
 
 @api.get("/edificacoes", response=List[EdificacaoOut])
 def list_edificacoes(request):
     qs = models.Edificacao.objects.all()
     return qs
+
+@api.get("/edificacoes/{cod_edificacao}", response=EdificacaoOut)
+def get_edificacao(request, cod_edificacao: str):
+    qs = get_object_or_404(models.Edificacao, codigo=cod_edificacao)
+    return qs
+
+@api.post("/edificacoes")
+def create_edificacao(request, payload: EdificacaoIn):
+    edificacao = models.Edificacao.objects.create(**payload.dict())
+    return {"success": True}
 
 @api.put("/edificacoes/{cod_edificacao}")
 def update_edificacoes(request, cod_edificacao: str, payload: EdificacaoIn):
