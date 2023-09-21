78d0abb4e73c29126d055754b34e7c975d1d1ad2
vynijales <vynijales@gmail.com>
2023-09-21 06:36:45 -0300

    update: sync_edificacoes

--- src/datasync/api_utils.py
+++ src/datasync/api_utils.py
@@ -9,3 +9,11 @@ def enviar_dados_para_api(api_url, csrf_token, data):
     json_data = json.dumps(data)
     response = requests.post(api_url, data=json_data, headers=headers)
     return response
+
+def receber_dados_da_api(api_url, csrf_token):
+    headers = {
+        'X-CSRFToken': csrf_token,
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(api_url, headers=headers)
+    return response
