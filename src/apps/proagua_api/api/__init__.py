ccbbd5351a98cd924ed33aadea8708a296d3d28c
vynijales <vynijales@gmail.com>
2023-09-21 05:46:13 -0300

    feat: criar endpoint para csrf token

--- src/apps/proagua_api/api/__init__.py
+++ src/apps/proagua_api/api/__init__.py
@@ -1,10 +1,12 @@
 from ninja import NinjaAPI
 from ninja.security import django_auth
+import requests
 
-from . import edificacoes, pontos, coletas, sequencia_coletas
+from . import csrf, edificacoes, pontos, coletas, sequencia_coletas
 
-api = NinjaAPI(auth=django_auth, csrf=True)
-
+# api = NinjaAPI(auth=django_auth, csrf=True)
+api = NinjaAPI(auth=None, csrf=False)
+api.add_router("/csrf", csrf.router)
 api.add_router("/edificacoes", edificacoes.router)
 api.add_router("/pontos", pontos.router)
 api.add_router("/sequencias", sequencia_coletas.router)
---
+++ src/apps/proagua_api/api/csrf.py
@@ -0,0 +1,9 @@
+from ninja import Router
+from django.middleware.csrf import get_token
+
+router = Router()
+
+@router.get("/")
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return {"csrf_token": csrf_token}
