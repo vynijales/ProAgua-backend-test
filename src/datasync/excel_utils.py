5e15264d0fe9bcc810f99ac0e2137582770e2821
vynijales <vynijales@gmail.com>
2023-09-22 12:59:58 -0300

    update: file_path declarada na função

--- src/datasync/excel_utils.py
+++ src/datasync/excel_utils.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import os
 
-def obter_dados_excel(file_path):
+def obter_dados_excel():
+    address = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(address, 'SI para PowerBi.xlsm')
     pd.set_option('display.max_columns', None)
     df = pd.read_excel(file_path, sheet_name='Banco de Dados', engine='openpyxl')
     return df
