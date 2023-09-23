fe72e8e27956be4c48d0e0993a40ebf77889a187
vynijales <vynijales@gmail.com>
2023-09-22 22:14:18 -0300

    feat: seed pontos de coleta

--- src/apps/proagua_api/management/commands/seed.py
+++ src/apps/proagua_api/management/commands/seed.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from datasync.excel_utils import obter_dados_excel
 
-from apps.proagua_api.models.edificacao import Edificacao
+from apps.proagua_api import models
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
@@ -16,12 +16,12 @@ class Command(BaseCommand):
     help = 'Seed data for the application'
 
     def create_edificacao(self, cronograma, codigo, nome, campus):
-        codigos_existentes = set(Edificacao.objects.values_list('codigo', flat=True))
+        codigos_existentes = set(models.Edificacao.objects.values_list('codigo', flat=True))
 
         if codigo in codigos_existentes:
             self.stdout.write(f"Dados já existentes para código {codigo}")
         else:
-            edificacao = Edificacao.objects.create(
+            edificacao = models.Edificacao.objects.create(
                 cronograma=cronograma,
                 codigo=codigo,
                 nome=nome,
@@ -54,9 +54,61 @@ class Command(BaseCommand):
 
             self.create_edificacao(cronograma, codigo, nome, campus)
 
+    def create_ponto(self, edificacao, ambiente, tipo, amontante = None):
+        # Checa se o ponto existe, tendo como parâmetros edificacao, ambiente, tipo e amontante como chaves compostas
+        ponto_exists = models.PontoColeta.objects.filter(
+            edificacao=edificacao,
+            ambiente=ambiente,
+            tipo=tipo,
+            amontante=amontante
+        ).exists()
+
+        if ponto_exists:
+            self.stdout.write(f"Já existe um ponto com esses atributos: "
+                                                 f"Edificacao: {edificacao}, "
+                                                 f"Ambiente: {ambiente}, "
+                                                 f"Tipo: {tipo}, "
+                                                 f"Amontante: {amontante}")
+        else:
+            # Cria um novo objeto PontoColeta
+            ponto = models.PontoColeta.objects.create(
+                edificacao=edificacao,
+                ambiente=ambiente,
+                tipo=tipo,
+                amontante=amontante
+            )
+
+            if ponto:
+                self.stdout.write(self.style.SUCCESS(f"{ponto} criado com sucesso."))
+            else:
+                self.stdout.write(self.style.ERROR("Erro ao criar o {ponto}."))  
+    
+    def create_pontos(self):
+        # Criação de pontos de coleta
+
+        df = obter_dados_excel("Lista de pontos")
+
+        for indice, linha in df.iterrows():
+            if indice <= 5:
+                continue
+
+            edificacao = models.Edificacao.objects.get(codigo=linha.iloc[0])
+
+            if 'bebedouro' in str(linha.iloc[3]):
+                tipo = 0
+            elif 'torneira' in str(linha.iloc[3]):
+                tipo = 1
+            else:
+                continue
+
+            ambiente = linha.iloc[4]
+            # tombo = linha.iloc[5]
+
+            self.create_ponto(edificacao, ambiente, tipo)
+
     def handle(self, *args, **options):
-        
         self.create_edificacoes()
+        self.create_pontos()
         self.stdout.write(self.style.SUCCESS('Seed data created successfully.'))
 
 if __name__ == '__main__':
