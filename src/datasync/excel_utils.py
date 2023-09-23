81b0b95df43637d5e2c824d840094b40adf54407
vynijales <vynijales@gmail.com>
2023-09-22 22:08:19 -0300

    update: generalizar função

--- src/datasync/excel_utils.py
+++ src/datasync/excel_utils.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 
-def obter_dados_excel():
+def obter_dados_excel(file_name = 'SI para PowerBi.xlsm', sheet_name = 'Banco de Dados'):
     address = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(address, 'SI para PowerBi.xlsm')
+    file_path = os.path.join(address, file_name)
     pd.set_option('display.max_columns', None)
-    df = pd.read_excel(file_path, sheet_name='Banco de Dados', engine='openpyxl')
+    df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
     return df
