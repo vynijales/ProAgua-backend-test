413179a16a7bf42e36c36657c408a51193b0f0c5
vynijales <vynijales@gmail.com>
2023-09-24 20:48:20 -0300

    update: file_name atualizado

--- src/datasync/excel_utils.py
+++ src/datasync/excel_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-def obter_dados_excel(file_name = 'SI para PowerBi.xlsm', sheet_name = 'Banco de Dados'):
+def obter_dados_excel(file_name = 'SI para PowerBi atualizada.xlsm', sheet_name = 'Banco de Dados'):
     address = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(address, file_name)
     pd.set_option('display.max_columns', None)
