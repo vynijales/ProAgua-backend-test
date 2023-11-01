3cc7fe9151456131bd2a6f6535f974f238940583
Thiago Coelho <thiago.coelho158@gmail.com>
2023-11-01 18:13:31 -0300

    refactor: refatorar models/ponto_coleta.py

--- src/apps/proagua_api/models/ponto_coleta.py
+++ src/apps/proagua_api/models/ponto_coleta.py
@@ -1,29 +1,38 @@
 from django.db import models
 
+TIPOS_PONTOS = (
+    (1, "Bebedouro"),
+    (2, "Reservatório predial superior"),
+    (3, "Reservatório predial inferior"),
+    (4, "Reservatório de distribuição superior"),
+    (5, "Reservatório de distribuição inferior"),
+    (6, "CAERN")
+)
+
 class PontoColeta(models.Model):
     id = models.AutoField(primary_key=True)
+
     edificacao = models.ForeignKey(
         to="Edificacao",
         related_name="PontoColeta",
         verbose_name="código da edificação", 
         on_delete=models.PROTECT,
-        blank=False)
+        blank=False
+    )
+    
     ambiente = models.CharField(
         verbose_name="ambiente",
-        max_length=20)
+        max_length=20
+    )
+    
     tipo = models.IntegerField(
         verbose_name="tipo",
-        choices=(
-            (1, "Bebedouro"),
-            (2, "Reservatório predial superior"),
-            (3, "Reservatório predial inferior"),
-            (4, "Reservatório de distribuição superior"),
-            (5, "Reservatório de distribuição inferior"),
-            (6, "CAERN")
-        ),
+        choices=TIPOS_PONTOS,
         default=(1, "Bebedouro")
     )
+    
     tombo = models.CharField(max_length=20, verbose_name="tombo", blank=True, null=True)
+
     amontante = models.ForeignKey(
         to="PontoColeta",
         verbose_name="ponto amontante",
