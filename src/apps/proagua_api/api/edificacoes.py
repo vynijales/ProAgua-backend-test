ca5fd14391803d143f145bec70d51929429b8911
Thiago Coelho <thiago.coelho158@gmail.com>
2023-11-07 18:48:24 -0300

    formatar arquivos

--- src/apps/proagua_api/api/edificacoes.py
+++ src/apps/proagua_api/api/edificacoes.py
@@ -10,22 +10,26 @@ from .. import models
 
 router = Router()
 
+
 @router.get("/", response=List[EdificacaoOut], tags=["Edificacoes"])
 @paginate
 def list_edificacoes(request):
     qs = models.Edificacao.objects.all()
     return qs
 
+
 @router.get("/{cod_edificacao}", response=EdificacaoOut, tags=["Edificacoes"])
 def get_edificacao(request, cod_edificacao: str):
     qs = get_object_or_404(models.Edificacao, codigo=cod_edificacao)
     return qs
+
 
 @router.post("/", tags=["Edificacoes"])
 def create_edificacao(request, payload: EdificacaoIn):
     edificacao = models.Edificacao.objects.create(**payload.dict())
     edificacao.save()
     return {"success": True}
+
 
 @router.put("/{cod_edificacao}", tags=["Edificacoes"])
 def update_edificacoes(request, cod_edificacao: str, payload: EdificacaoIn):
@@ -35,11 +39,13 @@ def update_edificacoes(request, cod_edificacao: str, payload: EdificacaoIn):
     edificacao.save()
     return {"success": True}
 
+
 @router.delete("/{cod_edificacao}", tags=["Edificacoes"])
 def delete_edificacao(request, cod_edificacao: str):
     edificacao = get_object_or_404(models.Edificacao, codigo=cod_edificacao)
     edificacao.delete()
     return {"success": True}
+
 
 @router.get("/{cod_edificacao}/pontos", response=List[PontoColetaOut], tags=["Edificacoes"])
 def list_pontos(request, cod_edificacao: str):
--- src/apps/proagua_api/api/fluxos.py
+++ src/apps/proagua_api/api/fluxos.py
@@ -9,16 +9,19 @@ from apps.proagua_api.models.fluxo import Fluxo
 
 router = Router()
 
+
 @router.get("/", response=List[FluxoOut], tags=["Fluxos"])
 @paginate
 def list_fluxo(request):
     qs = Fluxo.objects.all()
     return qs
 
+
 @router.get("/{id_fluxo}", response=FluxoOut, tags=["Fluxos"])
 def get_fluxo(request, id_fluxo: int):
     fluxo = get_object_or_404(Fluxo, id=id_fluxo)
     return fluxo
+
 
 @router.post("/", response=FluxoOut, tags=["Fluxos"])
 def create_fluxo(request, data: FluxoIn):
@@ -27,6 +30,7 @@ def create_fluxo(request, data: FluxoIn):
     fluxo.save()
     return fluxo
 
+
 @router.put("/{id_fluxo}", tags=["Fluxos"])
 def update_fluxo(request, id_fluxo: int, payload: FluxoIn):
     fluxo = get_object_or_404(Fluxo, id=id_fluxo)
@@ -34,6 +38,7 @@ def update_fluxo(request, id_fluxo: int, payload: FluxoIn):
         setattr(fluxo, attr, value)
     fluxo.save()
     return {"status": "success"}
+
 
 @router.delete("/{id_fluxo}", tags=["Fluxos"])
 def delete_fluxo(request, id_fluxo: int):
