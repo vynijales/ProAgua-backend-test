0a2c9f266b74e47ddcc0ba7ca1363bd436b893e8
Thiago Coelho <thiago.coelho158@gmail.com>
2023-11-09 12:07:19 -0300

    remove: remove links field from ponto_coleta endpoint (API)

--- src/apps/proagua_api/api/schemas/ponto_coleta.py
+++ src/apps/proagua_api/api/schemas/ponto_coleta.py
@@ -24,7 +24,6 @@ class PontoColetaOut(Schema):
     edificacao_url: str
     fluxos_url: str
     
-    links: dict = {}
 
     @staticmethod
     def resolve_edificacao_url(self):
@@ -33,16 +32,6 @@ class PontoColetaOut(Schema):
     @staticmethod
     def resolve_fluxos_url(self):
         return reverse("api-1.0.0:get_fluxos", kwargs={"id_ponto": self.id})
-
-    @staticmethod
-    def resolve_links(obj: models.PontoColeta):
-        return {
-            "edificacao": { 
-                "codigo_edificacao": obj.edificacao.codigo,
-                "url_edificacao": reverse("api-1.0.0:get_edificacao", kwargs={"cod_edificacao":obj.edificacao.codigo})
-            }
-        }
-
 
 class FilterPontos(FilterSchema):
     q: Optional[str] = Field(
