bab1e40c2c33438f3e694d8fac29fa65e01619c9
Thiago Coelho <thiago.coelho158@gmail.com>
2023-12-06 17:51:43 -0300

    feat: adicionar campo de coletas em sequencias (API)

--- src/apps/proagua_api/api/schemas/sequencia_coletas.py
+++ src/apps/proagua_api/api/schemas/sequencia_coletas.py
@@ -1,10 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 
 from ninja import Schema
 from django.urls import reverse
 
 from apps.proagua_api.models import PontoColeta
-
+from .coleta import ColetaOut
 
 class SequenciaColetasIn(Schema):
     amostragem: int
@@ -14,6 +14,7 @@ class SequenciaColetasOut(Schema):
     id: int
     amostragem: int
     ponto_url: Optional[str]
+    coletas: List[ColetaOut]
 
     @staticmethod
     def resolve_ponto_url(obj):
@@ -24,4 +25,3 @@ class SequenciaColetasOut(Schema):
             return reverse("api-1.0.0:get_ponto", kwargs={"id_ponto": ponto.id})
         
         return None
-
