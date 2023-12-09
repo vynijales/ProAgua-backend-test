bea57ff5ac124d09962ad22433eac1507ada8124
vynijales <vynijales@gmail.com>
2023-12-08 22:44:43 -0300

    feat: criar coleta pela interface

--- src/apps/frontend/static/js/criar_coleta.js
+++ src/apps/frontend/static/js/criar_coleta.js
@@ -1,3 +1,5 @@
+let isCriando = false;
+
 async function fetchJson(url) {
     const response = await fetch(url);
     if (!response.ok) {
@@ -15,8 +17,7 @@ async function carregarOpcoesPonto() {
 
         pontos.items.forEach(ponto => {
             let newOption = document.createElement("option");
-            newOption.id = ponto.id;
-            newOption.value = `${ponto.id} - ${ponto.ambiente} | ${ponto.tipo == '1' ? 'Bebedouro | ' + ponto.tombo : 'RPS'}`;
+            newOption.value = `${ponto.id} | ${ponto.ambiente} | ${ponto.tipo == '1' ? 'Bebedouro | ' + ponto.tombo : 'RPS'}`;
             document.getElementById("ponto").appendChild(newOption);
         });
 
@@ -32,10 +33,9 @@ async function carregarOpcoesResponsaveis() {
 
         responsavelSelect.innerHTML = "";
 
-        console.log(responsaveis);
         responsaveis.items.forEach(responsavel => {
             let newOption = document.createElement("option");
-            newOption.id = responsavel.id;
+            newOption.value = responsavel.id;
             newOption.innerText = responsavel.username;
             document.getElementById("responsaveis").appendChild(newOption);
         });
@@ -44,6 +44,73 @@ async function carregarOpcoesResponsaveis() {
         console.error('Erro ao carregar opções de responsáveis:', error);
     }
 }
+
+async function criarColeta() {
+    if (isCriando) {
+        console.log("Já existe uma requisição em andamento");
+        return;
+    }
+
+    isCriando = true;
+    const bt_criar = document.getElementById("criar");
+    bt_criar.disabled = true;
+    bt_criar.innerHTML = "Criando...";
+
+    const sequencia = document.getElementById("sequencia").value;
+    const ponto_coleta = document.getElementById("ponto_value").value;
+    const ponto_coleta_id = parseInt(ponto_coleta.split(" | ")[0]);
+    const temperatura = document.getElementById("temperatura").value;
+    const cloro = document.getElementById("cloro").value;
+    const turbidez = document.getElementById("turbidez").value;
+    const coliformes = document.getElementById("coliformes").checked;
+    const escherichia = document.getElementById("escherichia").checked;
+    const cor = document.getElementById("cor").value;
+    const data = document.getElementById("data").value;
+    const hora = document.getElementById("hora").value;
+    const responsaveis = [];
+    document.querySelectorAll("select[name='responsaveis'] option:checked").forEach(option => responsaveis.push(option.value));
+
+    const ordem = document.getElementById("ordem").value;
+
+    const json = {
+        "sequencia_id": sequencia,
+        "ponto_id": ponto_coleta_id,
+        "temperatura": temperatura,
+        "cloro_residual_livre": cloro,
+        "turbidez": turbidez,
+        "coliformes_totais": coliformes,
+        "escherichia": escherichia,
+        "cor": cor,
+        "data": data + "T" + hora + ":00Z",
+        "responsavel": responsaveis,
+        "ordem": ordem
+    };
+
+    const responseColeta = await fetch("/api/v1/coletas/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(json),
+    });
+
+    if (!responseColeta.ok) {
+        const errorResponse = await responseColeta.json();
+        throw new Error(`Erro ao criar a coleta: ${responseColeta.statusText}. Detalhes: ${JSON.stringify(errorResponse)}`);
+    }
+
+    window.location.href = "/ponto";
+    console.log("Coleta criada com sucesso!");
+
+
+    isCriando = false;
+    bt_criar.disabled = false;
+    bt_criar.innerHTML = "Criar";
+}
+
+// Adicionando um event listener ao botão
+document.getElementById("criar").addEventListener("click", criarColeta);
+
 
 carregarOpcoesPonto();
 carregarOpcoesResponsaveis();
