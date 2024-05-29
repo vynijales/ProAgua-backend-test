0aff3e4486e6e0010948626bb2e27011a3f65ad9
vynijales <vynijales@gmail.com>
2024-05-29 05:41:43 -0300

    feat: criar signal para que os "reservatorios associados" façam referência entre si da forma correta em nível de banco de dados

--- src/apps/proagua_api/apps.py
+++ src/apps/proagua_api/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProaguaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.proagua_api'
+
+    def ready(self):
+        import apps.proagua_api.signals
---
+++ src/apps/proagua_api/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from .models import PontoColeta
+
+@receiver(m2m_changed, sender=PontoColeta.associados.through)
+def update_associados(sender, instance, action, reverse, model, pk_set, **kwargs):
+    if action == "post_add":
+        for pk in pk_set:
+            ponto = PontoColeta.objects.get(pk=pk)
+            if ponto != instance:  # Add this line
+                for associado in instance.associados.all():
+                    if associado != ponto and associado not in ponto.associados.all():  # Modify this line
+                        ponto.associados.add(associado)
+                if instance not in ponto.associados.all():
+                    ponto.associados.add(instance)
+    elif action == "post_remove":
+        for pk in pk_set:
+            ponto = PontoColeta.objects.get(pk=pk)
+            if ponto != instance:  # Add this line
+                for associado in instance.associados.all():
+                    if associado != ponto and associado in ponto.associados.all():  # Modify this line
+                        ponto.associados.remove(associado)
+                if instance in ponto.associados.all():
+                    ponto.associados.remove(instance)
