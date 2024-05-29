5b31aa7cb457cb83e6b27f58a57237300be9fd60
vynijales <vynijales@gmail.com>
2024-05-29 06:23:43 -0300

    refactor: comentarios

--- src/apps/proagua_api/signals.py
+++ src/apps/proagua_api/signals.py
@@ -2,12 +2,13 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from .models import PontoColeta
 
+# Signal para atualizar os pontos associados
 @receiver(m2m_changed, sender=PontoColeta.associados.through)
 def update_associados(sender, instance, action, reverse, model, pk_set, **kwargs):
     if action == "post_add":
         for pk in pk_set:
             ponto = PontoColeta.objects.get(pk=pk)
-            if ponto != instance:  # Add this line
+            if ponto != instance:  # Se o ponto não for ele mesmo
                 for associado in instance.associados.all():
                     if associado != ponto and associado not in ponto.associados.all():  # Modify this line
                         ponto.associados.add(associado)
@@ -16,7 +17,7 @@ def update_associados(sender, instance, action, reverse, model, pk_set, **kwargs
     elif action == "post_remove":
         for pk in pk_set:
             ponto = PontoColeta.objects.get(pk=pk)
-            if ponto != instance:  # Add this line
+            if ponto != instance:   # Se o ponto não for ele mesmo
                 for associado in instance.associados.all():
                     if associado != ponto and associado in ponto.associados.all():  # Modify this line
                         ponto.associados.remove(associado)
