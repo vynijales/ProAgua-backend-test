a69f195ad1f2ce7e3576df556287d9b98d8eff2f
Thiago Coelho <thiago.coelho158@gmail.com>
2024-06-20 21:57:04 -0300

    feat: configuracao para uso de postgresql

--- src/core/settings.py
+++ src/core/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
